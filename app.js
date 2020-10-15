90e060cc4fd87b9d1ea0873743d9b0f75dd6bf17
Duong <nhduong29@gmail.com>
2020-10-15 23:35:41 +0800

    sdf
    sdf

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
@@ -7,6 +8,7 @@ const swaggerDocument = YAML.load('./The-Social-Integration.v1.yaml');
 var port = process.env.PORT || 8080;
  
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/docs', express.static(path.join(__dirname, 'public')))
 
 app.listen(port,()=>{
     console.log("server is running on "+ port)
